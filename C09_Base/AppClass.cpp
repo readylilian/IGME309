0905780515cbd1f806853b1c2dc47c64485f83c2
Lily Ready (RIT Student) <lr4631@rit.edu>
2023-03-07 11:59:53 -0500

    Class work with C09_Base

--- C09_Base/AppClass.cpp
+++ C09_Base/AppClass.cpp
@@ -7,6 +7,8 @@ void Application::InitVariables(void)
 	vector3 v3Target = ZERO_V3;
 	vector3 v3Upward = AXIS_Y;
 	m_pCameraMngr->SetPositionTargetAndUpward(v3Position, v3Target, v3Upward);
+
+	m_pModelMngr->LoadModel("Walleye.obj");
 }
 void Application::Update(void)
 {
@@ -22,6 +24,26 @@ void Application::Update(void)
 	//Update Entity Manager
 	m_pEntityMngr->Update();
 
+	static float fDelta = 5.0f;
+	//Swap any of these for fDelta to see the effect
+
+	//Where the camera is standing
+	vector3 v3Postition(0.0f, 0.0f, 10.0f);
+	
+	//Where the camera is looking
+	vector3 v3Target(0.0f, 0.0f, 0.0f);
+
+	//Sets where UP is, so if y = -1, then everything is upside down
+	vector3 v3Upwards(1.0f, 0.0f, 0.0f);
+	
+	m_pCameraMngr->SetUpward(v3Upwards);
+	m_pCameraMngr->SetTarget(v3Target);
+	m_pCameraMngr->SetPosition(v3Postition);
+	fDelta += m_pSystem->GetDeltaTime(0);
+
+	//std::cout << fDelta << std::endl;
+
+	m_pModelMngr->AddModelToRenderList("Walleye.obj", ToMatrix4(m_qArcBall), BTX::eBTX_RENDER::RENDER_WIRE | BTX::eBTX_RENDER::RENDER_SOLID);
 	//Add objects to render list
 	m_pEntityMngr->AddEntityToRenderList(-1, true);
 }
